aedd6849bb7ed30306ea45a62fcf1d7059cf2a6a
Al Zee <z@alz.ee>
2021-12-19 15:21:36 +0800

    nth

---
+++ jj.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# vim:ft=sh
+
+############### Variables ###############
+
+############### Functions ###############
+
+############### Main Part ###############
+word=jj
+intv=20
+logfile=x_access.log
+dir=
+
+sum=$(grep -w $word $logfile | tail -1 | md5sum | awk '{print $1}')
+
+echo $sum
+
+sum0=$(redis-cli get sum0)
+
+if [ "$sum" != "$sum0" ]; then
+    echo sum0 updated.
+    redis-cli set sum0 $sum
+
+    date=$(date +%s)
+    date0=$(redis-cli get date0)
+    let diff=date-date0
+    # If have NOT sent in 1 hour
+    if [ $diff -gt $intv ]; then
+        echo date0 updated.
+        redis-cli set date0 $date
+
+        # Run php sms code
+        echo Run php sms code...
+    fi
+    echo $diff
+fi 
